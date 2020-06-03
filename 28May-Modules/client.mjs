1e40b21ebf6d2564f83d32b57ec3f47d7c02eeb2
DylanCHT <43015876+DylanCHT@users.noreply.github.com>
2020-06-02 22:00:48 -0400

    d

--- 28May-Modules/client.mjs
+++
@@ -1,5 +0,0 @@
-//This is the MJS style client
-
-import {adder as somethingFancier, PI} from "./newMath.mjs";
-
-console.log(`Sum: ${somethingFancier(40,2)} and PI is ${PI}`)
--- 28May-Modules/newMath.mjs
+++
@@ -1,6 +0,0 @@
-//MJS = 'Michael Jackson Script'
-
-export const PI = 3.14157;
-export const adder = (left, right) => left + right;
-const subber = (left, right) => left - right;
-
